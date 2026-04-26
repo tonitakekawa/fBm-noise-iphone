07296e96b05da6f82c6480be0b2dfe2373a295c5
tonitakekawa <tonitakekawa@gmail.com>
2026-04-26 15:15:46 +0900

    Initial Commit

---
+++ noise/noiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  noiseApp.swift
+//  noise
+//
+//  Created by tny on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct noiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
